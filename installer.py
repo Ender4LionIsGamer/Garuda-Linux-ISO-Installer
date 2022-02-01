92413c7977cd12ec4133d40c09cf3e8670895fec
Ender4LionIsGamer <98265648+Ender4LionIsGamer@users.noreply.github.com>
2022-02-01 19:20:51 +0530

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,19 @@
+print("Garuda Linux ISO Installer")
+pip install requests
+# imported the requests library
+import requests
+iso_url = "https://sourceforge.net/projects/garuda-linux/files/garuda/dr460nized/220101/garuda-dr460nized-linux-zen-220101.iso/download"
+  
+# URL of the image to be downloaded is defined as image_url
+r = requests.get(iso_url) # create HTTP response object
+  
+# send a HTTP request to the server and save
+# the HTTP response in a response object called r
+with open("garuda-dr460nized-linux-zen-220101.iso",'wb') as f:
+  
+    # Saving received content as a png file in
+    # binary format
+  
+    # write the contents of the response (r.content)
+    # to a new file in binary mode.
+    f.write(r.content)
